df06956986c0ee6ce92cf1ac97d8d4c7d5a8228f
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-01-22 14:06:32 +0530

    Create mvmedia.sh

---
+++ abusers/mvmedia.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source /home/rlksvrlogs/scripts/dataset.sh
+
+input=$1
+
+function check_input() {
+	username=$(cat /etc/trueuserowners | awk '{print $1}' | sed 's/://' | awk -v user=$input '{if ($1==user) print $1}')
+
+	if [[ $input == "$username" ]]; then
+		mv_media
+	else
+		echo "Username not found"
+	fi
+}
+
+function mv_media() {
+	fspath=($(find $svrlogs/abusers/filesharing -type f -name "$username-fs*" -exec ls -lat {} + | head -1 | awk '{print $NF}'))
+
+	if [ ! -z $fspath ]; then
+		filesharing=$(cat $fspath | awk '{$1=$2=$3=""; print}' | grep -v total$ | sed 's/^[[:space:]]*//')
+
+		if [[ ! -z $filesharing ]]; then
+			while IFS= read -r line; do
+				if [[ -f "$line" ]]; then
+					mv "$line" /home/$username/Abuse
+
+					echo "Moved - $line" >>$svrlogs/abusers/move/$username-mvfs_$time.txt
+				else
+					echo "Unavailable - $line" >>$svrlogs/abusers/move/$username-mvfs_$time.txt
+				fi
+
+			done <<<"$filesharing"
+		fi
+	else
+		echo "File list not found"
+	fi
+}
+
+check_input
