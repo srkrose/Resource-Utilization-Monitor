b567e0a29d26d93fe158b4a802b065a0cc0515a6
A. D. R. Lakshani <22644135+srkrose@users.noreply.github.com>
2024-01-22 14:31:13 +0530

    Update mvmedia.sh

--- abusers/mvmedia.sh
+++ abusers/mvmedia.sh
@@ -8,9 +8,21 @@ function check_input() {
 	username=$(cat /etc/trueuserowners | awk '{print $1}' | sed 's/://' | awk -v user=$input '{if ($1==user) print $1}')
 
 	if [[ $input == "$username" ]]; then
+		mk_dir
+		
 		mv_media
 	else
 		echo "Username not found"
+	fi
+}
+
+function mk_dir() {
+	filepath="/home/$username/Abuse"
+
+	if [[ ! -d "$filepath" ]]; then
+		mkdir $filepath
+
+		chown $username: $filepath
 	fi
 }
 
@@ -23,7 +35,7 @@ function mv_media() {
 		if [[ ! -z $filesharing ]]; then
 			while IFS= read -r line; do
 				if [[ -f "$line" ]]; then
-					mv "$line" /home/$username/Abuse
+					mv "$line" $filepath
 
 					echo "Moved - $line" >>$svrlogs/abusers/move/$username-mvfs_$time.txt
 				else
